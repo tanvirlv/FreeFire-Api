1a2d0debfc006cc96723a809f11829b05486958c
LV <tanvir12421@gmail.com>
2025-11-02 16:36:32 +0600

    Update app.py

--- app.py
+++ app.py
@@ -375,4 +375,4 @@ def get_account_info():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True, host='0.0.0.0', port=port)
